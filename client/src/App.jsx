b4bea86fcdfef60f40ae1915ec8bfb02fd5e7ace
code-commit-demo <satyadav941@gmail.com>
2024-01-25 19:40:04 +0530

    react router added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,19 @@
 import Navbar from "./components/Navbar";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignUp from "./components/SignUp";
+import Login from "./components/Login";
 function App() {
   return <>
-  <Navbar />
+  
+    <BrowserRouter>
+        <Navbar/>
+      <Routes>
+        <Route path="/sign-up" element={<SignUp/>}
+        />
+        <Route path="/sign-in" element={<Login/>}
+        />
+      </Routes>
+    </BrowserRouter>
   </>;
 }
 
